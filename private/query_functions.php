24c925d11c9721b1e142b9fc4615e8c1bd3f0d34
HypeByte <HypeByteProgrammer@gmail.com>
2022-07-26 22:44:11 -0700

    Added public navigation bar in public section for navigation of pages by each subject

--- private/query_functions.php
+++ private/query_functions.php
@@ -277,4 +277,15 @@
     }
   }
 
+  function find_pages_by_subject_id($subject_id) {
+    global $db;
+
+    $sql = "SELECT * FROM pages ";
+    $sql .= "WHERE subject_id='" . db_escape($db, $subject_id) . "' ";
+    $sql .= "ORDER BY position ASC";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    return $result; // returns an assoc. array
+  }
+
 ?>
--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -16,8 +16,8 @@
       <h1>GBI Staff Area</h1>
     </header>
 
-    <navigation>
+    <nav>
       <ul>
         <li><a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
       </ul>
-    </navigation>
+    </nav>
