4f62bd4ee5067e7d9256513839f7494f4b44b509
HypeByte <HypeByteProgrammer@gmail.com>
2022-07-26 23:49:30 -0700

    Added page content and page navigation in public area of website

--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -1,16 +1,24 @@
+<?php
+$subject_id = $subject_id ?? '';
+$page_id = $page_id ?? '';
+
+?>
+
+
+
 <nav>
   <?php $nav_subjects = find_all_subjects(); ?>
   <ul class="subjects">
     <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) { ?>
-      <li>
+      <li class="<?php if($nav_subject['id'] == $subject_id) {echo 'selected';} ?>">
         <a href="<?php echo url_for('index.php'); ?>">
           <?php echo h($nav_subject['menu_name']); ?>
         </a>
           <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); ?>
           <ul class="pages">
               <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
-                  <li>
-                      <a href="<?php echo url_for('index.php'); ?>">
+                  <li class="<?php if($nav_page['id'] == $page_id) {echo 'selected';} ?>">
+                      <a href="<?php echo url_for('index.php?id=' . h(u($nav_page['id']))); ?>">
                           <?php echo h($nav_page['menu_name']); ?>
                       </a>
                   </li>
--- public/index.php
+++ public/index.php
@@ -1,10 +1,31 @@
 <?php require_once('../private/initialize.php'); ?>
 
+<?php
+
+if(isset($_GET['id'])) {
+  $page_id = $_GET['id'];
+  $page = find_page_by_id($page_id);
+  if(!$page) {
+    redirect_to(url_for('/index.php'));
+  }
+  $subject_id = $page['subject_id'];
+}
+
+?>
+
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="main">
   <?php include(SHARED_PATH . '/public_navigation.php'); ?>
   <div id="page">
+
+      <?php
+        if(isset($page)) {
+            echo $page['content'];
+        } else {
+            include(SHARED_PATH . '/static_homepage.php');
+        }
+      ?>
 
   </div>
 
